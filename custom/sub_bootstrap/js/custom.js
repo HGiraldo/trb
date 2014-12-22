0dfc4f43508c3321d68073a59623b7560bb457cc
Harold Giraldo <haroldgiraldorestrepo@gmail.com>
2014-12-22 00:04:18 -0500

    Themes Styles

--- custom/sub_bootstrap/js/custom.js
+++ custom/sub_bootstrap/js/custom.js
@@ -2,6 +2,24 @@
 
   Drupal.custom = Drupal.custom || {};
 
+  // Validar La url del Video vía ajax (php) y traer el resultado embebido.
+  Drupal.AddProfile = function(element, val) {
+    $this = element;
+    if ($this) {
+      $.ajax({
+        url: Drupal.settings.basePath + "post/ajax/verifuser",
+        type: "POST",
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+          if (data) {
+            $this.append(data);
+          }
+        }
+      });
+    }
+    else {}
+  };
+
   Drupal.behaviors.custom = {
     attach:function(context, settings) {
       // LIST-DIRECTORY
@@ -10,6 +28,18 @@
           valueNames: [ 'name', 'cargo', 'ext', 'mail' ]
         };
         var userList = new List('rb-directory', options);
+      });
+
+      $('#widget_pager_bottom_news_home-block').once('pager', function() {
+        $this = $(this);
+        var clone_text = $('.view-footer').find('.more-news-div').clone();
+        $('.view-footer').find('.more-news-div').hide();
+        $this.parent().append(clone_text);
+      });
+
+      $('.logged-in, .not-logged-in').once('loggedin', function() {
+        $('.navbar-header').css('width', '100%');
+        Drupal.AddProfile($('.navbar-header'));
       });
 
       // BARRA LATERAL.
